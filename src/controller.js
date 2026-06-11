5ea8fa2cd47d1d2b0b1becac09783ff8ba27de04
Amit Shtark <amit.shtark@gmail.com>
2026-06-11 20:22:15 +0300

    added controller file

---
+++ src/controller.js
@@ -0,0 +1,61 @@
+const view = require("./view");
+const autoCompleteTrie = require("./autoCompleteTrie");
+const trie = new autoCompleteTrie();
+
+function handleCommand(input)
+{
+    const parts = input.split(" ");
+    const command = parts[0];
+    const word = parts[1];
+    switch (command) {
+    case "add": {
+        if (!word)
+        {
+            view.showError(`Missing word for ${command} command`);
+            break;
+        }
+        try {
+            trie.addWord(word);
+            view.showAdd(word);
+            break;
+        } catch (err) {
+            view.showError(err.message);
+            break;
+        }
+    }
+
+    case "complete": {
+        if (!word)
+        {
+            view.showError(`Missing word for ${command} command`);
+            break;
+        }
+        allWords = trie.predictWords(word);
+        view.showSuggestions(word, allWords);
+        break;
+    }
+
+    case "find": {
+        if (!word)
+        {
+            view.showError(`Missing word for ${command} command`);
+            break;
+        }
+        if(trie.findWord(word))
+            view.showFound(word);
+        else
+            view.showNotFound(word);
+        break;
+    }
+
+    case "help":
+        view.showHelp();
+        break;
+
+    default:
+        view.showError(`Unknown command '${command}'`);
+        console.log(
+        );
+    }
+}
+module.exports = {handleCommand};
