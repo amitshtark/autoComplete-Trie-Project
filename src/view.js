0a6e5e9b115aa6a7bedfaeab792a2b0a0558eb08
Amit Shtark <amit.shtark@gmail.com>
2026-06-11 20:30:19 +0300

    fixed false gaps in the showHelp function

--- src/view.js
+++ src/view.js
@@ -24,9 +24,8 @@ function showNotFound(word) {
 }
 
 function showHelp() {
-  console.log(`
-
-Commands:
+  
+console.log(`Commands:
     add <word>              - Add word to dictionary
     find <word>             - Check if word exists
     complete <prefix>       - Get completions
